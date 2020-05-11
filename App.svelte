44a342ec6e7b80eaec8c226bd6ee50b4364ca0a7
awm70 <46494258+awm70@users.noreply.github.com>
2020-05-11 21:36:02 +0700

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Todos from "./Todos.svelte";
+  import ApplloClient from "apollo-boost";
+  import { setClient } from "svelte-apollo";
+  const client = new ApplloClient({
+    uri: "https://todos-graphql.herokuapp.com/graphql"
+  });
+  setClient(client);
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Svelte Todos</h1>
+	<Todos />
+</main>
+
